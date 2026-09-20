abee0e60e6e1409c4666578fa6f952536983290f
somagouda <madagoudasomagouda@gmail.com>
2026-09-20 23:06:39 +0530

    tictac

--- IntroReact/for/src/App.jsx
+++ IntroReact/for/src/App.jsx
@@ -5,17 +5,30 @@ import viteLogo from './assets/vite.svg'
 import './App.css'
 import Calculator from './Calculator'
 function App() {
-  function handleClick() {
-    alert('Button clicked!')
+   const [board, setBoard] = useState(Array(9).fill(""));
 
-  }
-  function onmouseover() {
-    alert('Mouse over event triggered!')
-  }
+  function handleClick(index) {
+    const newBoard = [...board];
 
+    newBoard[index] = "X";
+
+    setBoard(newBoard);
+  }
+  
   return (
     <div>
-      <Calculator />
+      <h1>Tic Tac Toe</h1>
+
+      <div className="board">
+        {board.map((value, index) => (
+          <button
+            key={index}
+            onClick={() => handleClick(index)}
+          >
+            {value}
+          </button>
+        ))}
+      </div>
     </div>
    
   )
