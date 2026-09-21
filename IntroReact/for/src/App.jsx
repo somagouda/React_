9186e0e42ab45426fecd428a78c4945cead085f3
somagouda <madagoudasomagouda@gmail.com>
2026-09-22 00:53:04 +0530

    react forms

--- IntroReact/for/src/App.jsx
+++ IntroReact/for/src/App.jsx
@@ -3,34 +3,54 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg' 
 import viteLogo from './assets/vite.svg'
 import './App.css'
-import Calculator from './Calculator'
+import{createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from './components/Home.jsx';
+import About from './components/About.jsx';
+import Dashboard from './components/Dashboard.jsx';
+import Navbar from './components/Navbar.jsx';
+import { useForm } from 'react-hook-form';
+
+
 function App() {
-   const [board, setBoard] = useState(Array(9).fill(""));
-
-  function handleClick(index) {
-    const newBoard = [...board];
-
-    newBoard[index] = "X";
-
-    setBoard(newBoard);
-  }
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  
+function onSubmit(data) {
+  alert("Form submitted successfully!");
+  console.log(data);
+}
+function onError(errors) {
+  alert("Form submission failed. Please check the input fields.");
+}
+ 
+ 
   
   return (
-    <div>
-      <h1>Tic Tac Toe</h1>
+   <form onSubmit={handleSubmit(onSubmit, onError)}>
 
-      <div className="board">
-        {board.map((value, index) => (
-          <button
-            key={index}
-            onClick={() => handleClick(index)}
-          >
-            {value}
-          </button>
-        ))}
+      <div>
+        <label>First Name</label>
+        <input {...register("firstName", { required: true, minLength:{ value: 2, message: "First name is required and must be at least 2 characters long" } })} />  
       </div>
-    </div>
+       <div>
+        <label>Middle Name</label>
+        <input {...register("middleName")} />  
+      </div>
+      <br/>
+       <div>
+        <label>Last Name</label>
+        <input {...register("lastName")} />  
+      </div>
+      <br/>
+      <input type="submit" />
+
+    </form>
    
+    
   )
 }
 
