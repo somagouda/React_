3a052bfd4017ae37856c33f920bd76edb564ce2d
somagouda <madagoudasomagouda@gmail.com>
2026-09-24 00:59:08 +0530

    useref

--- IntroReact/for/src/App.jsx
+++ IntroReact/for/src/App.jsx
@@ -1,57 +1,42 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg' 
-import viteLogo from './assets/vite.svg'
-import './App.css'
-import{createBrowserRouter, RouterProvider} from "react-router-dom";
-import Home from './components/Home.jsx';
-import About from './components/About.jsx';
-import Dashboard from './components/Dashboard.jsx';
-import Navbar from './components/Navbar.jsx';
-import { useForm } from 'react-hook-form';
-
+import { useRef } from 'react';
+import './App.css';
 
 function App() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
-  
-function onSubmit(data) {
-  alert("Form submitted successfully!");
-  console.log(data);
-}
-function onError(errors) {
-  alert("Form submission failed. Please check the input fields.");
-}
- 
- 
-  
+  let btnRef = useRef();
+  let input1Ref = useRef();
+  let input2Ref = useRef();
+
+  function changecolor() {
+    btnRef.current.style.backgroundColor = "red";
+
+    // Get text from first input
+    let text = input1Ref.current.value;
+
+    // Put text into second input
+    input2Ref.current.value = text;
+  }
+
   return (
-   <form onSubmit={handleSubmit(onSubmit, onError)}>
+    <div>
+      <input
+        ref={input1Ref}
+        type="text"
+        placeholder="copy"
+      />
 
-      <div>
-        <label>First Name</label>
-        <input {...register("firstName", { required: true, minLength:{ value: 2, message: "First name is required and must be at least 2 characters long" } })} />  
-      </div>
-       <div>
-        <label>Middle Name</label>
-        <input {...register("middleName")} />  
-      </div>
-      <br/>
-       <div>
-        <label>Last Name</label>
-        <input {...register("lastName")} />  
-      </div>
-      <br/>
-      <input type="submit" />
+      <button ref={btnRef} onClick={changecolor}>
+        Copy
+      </button>
 
-    </form>
-   
-    
-  )
+      <br />
+
+      <input
+        ref={input2Ref}
+        type="text"
+        placeholder="paste"
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
